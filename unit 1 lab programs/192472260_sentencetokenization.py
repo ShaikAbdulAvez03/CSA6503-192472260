f2cb3d2fe2aac92872cb056acf694f6fa8edf93f
ShaikAbdulAvez03 <192472260.simats@saveetha.com>
2026-07-22 20:27:21 +0530

    Delete unit 1 lab programs directory

--- unit 1 lab programs/192472260_sentencetokenization.py
+++
@@ -1,7 +0,0 @@
-import nltk
-from nltk.tokenize import sent_tokenize
-
-text = "Python is easy. It is used for AI. It is popular."
-
-sentences = sent_tokenize(text)
-print(sentences)
--- unit 1 lab programs/192472260_wordtokenization.py
+++
@@ -1,7 +0,0 @@
-import nltk
-from nltk.tokenize import word_tokenize
-
-text = "Python is easy to learn."
-
-words = word_tokenize(text)
-print(words)
